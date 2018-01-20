7fc0ee39d6a9d2468d2ea57353a091ef028eea17
Roscoe Sabale <rsabale43@gmail.com>
2018-01-20 09:05:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (int i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     array.append(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
   return array;
